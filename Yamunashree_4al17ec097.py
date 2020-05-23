9169bcee7bce33a324cb1f8aa79a49b390da534e
yamunashree3117 <65490211+yamunashree3117@users.noreply.github.com>
2020-05-23 20:09:34 +0530

    Create Yamunashree_4al17ec097.py

---
+++ Yamunashree_4al17ec097.py
@@ -0,0 +1,12 @@
+entire=0
+while entire<3:
+user=input('Enter your username:')
+pw=input('Enter your password:')
+if username=='Michael' and password=='83$WT89x 
+     print('successfully login')
+     Break
+else:
+print('invalid username and password')
+entire+=1
+if entire=3:
+print('account is locked') 
